b38ad3f7628f97dfb46d2e79dd4890b82d5b13d5
Leonardo Kulkamp <kulkamp.leonardo@escola.pr.gov.br>
2026-09-14 18:13:21 +0000

    14/09

--- TestaFuncionario.java
+++ TestaFuncionario.java
@@ -10,5 +10,18 @@ public class TestaFuncionario {
         System.out.println("CPF: " + leonardo.getCpf());
         System.out.println("Salário: R$ " + leonardo.getSalario());
         System.out.println("Bonificação: R$ " + leonardo.getBonificacao());
+
+        Gerente theo = new Gerente();
+        theo.setCpf("895.123.564-51");
+        theo.setNome("Theo Borges Oliveira");
+        theo.setSalario(9410);
+        theo.setTipo(1);
+        theo.setSenha(1241515);
+
+        System.out.println("\n O nome é: " + theo.getNome());
+        System.out.println("O CPF é: " + theo.getCpf());
+        System.out.println("O salário é: " +theo.getSalario());
+        System.out.println("A bonificação é: " + theo.getBonificacao());
+        System.out.println(theo.autentica(1241515));
     }
 }
